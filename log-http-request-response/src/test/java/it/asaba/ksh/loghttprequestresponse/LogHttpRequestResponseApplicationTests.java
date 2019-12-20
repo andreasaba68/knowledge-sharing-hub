65b17429fe741b1615ec460841d8ae5e23a6a94a
Andrea Saba <andreasaba@gmail.com>
2019-12-20 19:26:58 +0100

    first commit

---
+++ log-http-request-response/src/test/java/it/asaba/ksh/loghttprequestresponse/LogHttpRequestResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.asaba.ksh.loghttprequestresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHttpRequestResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
