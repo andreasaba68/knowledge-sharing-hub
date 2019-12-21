51d9f513d7c67910723121bf7cd104a0b9458cf3
Andrea Saba <andreasaba@gmail.com>
2019-12-21 17:49:13 +0100

    minor formatting & commenting

--- log-http-request-response/src/main/java/it/asaba/ksh/loghttprequestresponse/LoggingFilter.java
+++ log-http-request-response/src/main/java/it/asaba/ksh/loghttprequestresponse/LoggingFilter.java
@@ -19,57 +19,72 @@ import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
+/**
+ * Logging Filter
+ * @author andrea.saba
+ */
+
 @Configuration
 public class LoggingFilter extends GenericFilterBean {
 
-  /**
-   * It's important that you actually register your filter this way rather then just annotating it as @Component
-   * as you need to be able to set for which "DispatcherType"s to enable the filter (see point *1*)
-   * 
-   * @return
-   */
-  @Bean
-  public FilterRegistrationBean<LoggingFilter> initFilter(){
-      FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
-      registrationBean.setFilter(new LoggingFilter());
+    /**
+     * It's important that you actually register your filter this way rather then just annotating it
+     * as @Component as you need to be able to set for which "DispatcherType"s to enable the filter
+     * (see point *1*)
+     * 
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> initFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
 
-      // *1* make sure you sett all dispatcher types if you want the filter to log upon
-      registrationBean.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        // *1* make sure you sett all dispatcher types if you want the filter to log upon
+        registrationBean.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
 
-      // *2* this should put your filter above any other filter
-      registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        // *2* this should put your filter above any other filter
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
-      return registrationBean;    
-  }
+        return registrationBean;
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
-		ContentCachingRequestWrapper wreq= new ContentCachingRequestWrapper((HttpServletRequest)request);
-		ContentCachingResponseWrapper wres=new ContentCachingResponseWrapper((HttpServletResponse)response);
+        ContentCachingRequestWrapper wreq = new ContentCachingRequestWrapper(
+                (HttpServletRequest) request);
+        ContentCachingResponseWrapper wres = new ContentCachingResponseWrapper(
+                (HttpServletResponse) response);
 
-		try {
+        try {
 
-      // let it be ...
-			chain.doFilter(wreq, wres);
+            // let it be ...
+            chain.doFilter(wreq, wres);
 
-			while (wreq.getInputStream().read()>=0);
-      System.out.printf("=== REQUEST%n%s%n=== end request%n",new String(wreq.getContentAsByteArray()));
+            // makes sure that the input is read (e.g. in 404 it may not be)
+            while (wreq.getInputStream().read() >= 0);
 
-			// Do whatever logging you wish here, in this case I'm writing request and response to system out which is certanly what you do not want
-	    System.out.printf("=== RESPONSE%n%s%n=== end response%n",new String(wres.getContentAsByteArray()));
+            System.out.printf("=== REQUEST%n%s%n=== end request%n",
+                    new String(wreq.getContentAsByteArray()));
 
-	    // this is specific of the "ContentCachingResponseWrapper" we are relying on, make sure you call it after you read the content from the response
-      wres.copyBodyToResponse();
+            // Do whatever logging you wish here, in this case I'm writing request 
+            // and response to system out which is probably not what you wish to do
+            System.out.printf("=== RESPONSE%n%s%n=== end response%n",
+                    new String(wres.getContentAsByteArray()));
 
-	    // One more point, in case of redirect this will be called twice! beware to handle that somewhat
+            // this is specific of the "ContentCachingResponseWrapper" we are relying on, 
+            // make sure you call it after you read the content from the response
+            wres.copyBodyToResponse();
 
-		} catch (Throwable t) {
-      // Do whatever logging you whish here, too
-		  // TODO: here you should also be logging the error!!!
-			throw t;
-		}
+            // One more point, in case of redirect this will be called twice! beware to handle that
+            // somewhat
 
-	}
+        } catch (Throwable t) {
+            // Do whatever logging you whish here, too
+            // here you should also be logging the error!!!
+            throw t;
+        }
+
+    }
 }
